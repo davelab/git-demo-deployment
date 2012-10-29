caec9574c2f7aede28ad057096b91e18cf0998c3
davelab <info@davelab.net>
2012-10-29 14:04:55 +0100

    modified: contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
 <h1><?php echo  "Ciao questo è il tuo ip: " . $_SERVER['REMOTE_ADDR']; ?></h1>
 
-<h2>se visualizzi la seguente riga hai vinto:</h2>
+<h2>se visualizzi la seguente riga hai vinto: </h2>
 <p>e vieni da questa pagina <?php echo $_SERVER['HTTP_REFERER']; ?></p>
