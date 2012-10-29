0e30347fefedefac8553f5ff278d1670f911f7c9
davelab <info@davelab.net>
2012-10-30 00:25:48 +0100

    modified: contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,4 @@
 <h1><?php echo  "Ciao questo è il tuo ip: " . $_SERVER['REMOTE_ADDR']; ?></h1>
 
 
-<p>ora esatta a Roma: <?php echo date("hh:mm") ?></p>
-
-abbasta!!
+<p>ora esatta a Roma: <?php echo date(); ?></p>
