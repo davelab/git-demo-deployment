8c006e1f8f287e0553223b047471ea2a6464c469
davelab <info@davelab.net>
2012-10-29 13:47:23 +0100

    modified index.php contact link

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <![endif]-->
 
        <?php echo "<h1>Testo di prova</h1>" ?>
-       <a href="contac.php">Contatti</a>
+       <a href="contact.php">Contatti</a>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
